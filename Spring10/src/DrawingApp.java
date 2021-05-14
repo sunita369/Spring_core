b965fe03f1d8ffeb1e2bb834fb219661c33ce29f
sunita369 <sunita98mondal@gmail.com>
2021-05-14 20:53:42 +0530

    Spring core 10 has been added

---
+++ Spring10/src/DrawingApp.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class DrawingApp 
+{
+ 
+ public static void main(String[] args) 
+ 	{
+	 	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	 	Triangle triangle = (Triangle) context.getBean("triangle");
+	 	triangle.draw();
+ 	}
+}
+
